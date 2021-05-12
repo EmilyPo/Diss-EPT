511a4aa94e8d79b9199bbbfdddba5306d552c118
Emily D Pollock <emilypo@uw.edu>
2021-05-12 11:28:17 -0700

    committing simulation file before re-running with addt'l filter for no sex following ept flag

--- sims/Simulations.R
+++ sims/Simulations.R
@@ -13,15 +13,15 @@ sim.base.0 <- netsim(nets,
                    init = init_het_ct(),
                    control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.base.0, here("sims", "may", "simBase0.rds"))
+saveRDS(sim.base.0, here("sims", "may2", "simBase0.rds"))
 rm(sim.base.0)
 
 sim.base <- netsim(nets,
-                   param = param_het_ept(epistats, ept.coverage = 0.25, female.modifier = 4.5),
-                   init = init_het_ct(prev.uct.female.young = 0.0474, prev.uct.male.young = 0.03),
+                   param = param_het_ept(epistats, ept.coverage = 0.25),
+                   init = init_het_ct(),
                    control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.base, here("sims", "may", "simBase.rds"))
+saveRDS(sim.base, here("sims", "may2", "simBase.rds"))
 rm(sim.base)
 
 sim.base.ept <- netsim(nets,
@@ -29,7 +29,7 @@ sim.base.ept <- netsim(nets,
                        init = init_het_ct(),
                        control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.base.ept, here("sims", "may", "simBaseEPT.rds"))
+saveRDS(sim.base.ept, here("sims", "may2", "simBaseEPT.rds"))
 rm(sim.base.ept)
 
 sim.base.ept.high <- netsim(nets,
@@ -37,7 +37,7 @@ sim.base.ept.high <- netsim(nets,
                             init = init_het_ct(),
                             control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.base.ept.high, here("sims", "may", "simBaseEPThigh.rds"))
+saveRDS(sim.base.ept.high, here("sims", "may2", "simBaseEPThigh.rds"))
 rm(sim.base.ept.high)
 
 sim.base.ept.100 <- netsim(nets,
@@ -46,7 +46,7 @@ sim.base.ept.100 <- netsim(nets,
                             init = init_het_ct(),
                             control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.base.ept.100, here("sims", "may", "simBaseEPT100.rds"))
+saveRDS(sim.base.ept.100, here("sims", "may2", "simBaseEPT100.rds"))
 rm(sim.base.ept.100)
 rm(nets)
 
@@ -58,7 +58,7 @@ sim.avg.0 <- netsim(nets,
                      init = init_het_ct(),
                      control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.avg.0, here("sims", "may", "simAvg0.rds"))
+saveRDS(sim.avg.0, here("sims", "may2", "simAvg0.rds"))
 rm(sim.avg.0)
 
 sim.avg <- netsim(nets,
@@ -66,7 +66,7 @@ sim.avg <- netsim(nets,
                    init = init_het_ct(),
                    control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.avg, here("sims", "may", "simAvg.rds"))
+saveRDS(sim.avg, here("sims", "may2", "simAvg.rds"))
 rm(sim.avg)
 
 sim.avg.ept <- netsim(nets,
@@ -74,7 +74,7 @@ sim.avg.ept <- netsim(nets,
                        init = init_het_ct(),
                        control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.avg.ept, here("sims", "may", "simAvgEPT.rds"))
+saveRDS(sim.avg.ept, here("sims", "may2", "simAvgEPT.rds"))
 rm(sim.avg.ept)
 
 sim.avg.ept.high <- netsim(nets,
@@ -82,7 +82,7 @@ sim.avg.ept.high <- netsim(nets,
                             init = init_het_ct(),
                             control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.avg.ept.high, here("sims", "may", "simAvgEPThigh.rds"))
+saveRDS(sim.avg.ept.high, here("sims", "may2", "simAvgEPThigh.rds"))
 rm(sim.avg.ept.high)
 
 sim.avg.ept.100 <- netsim(nets,
@@ -91,7 +91,7 @@ sim.avg.ept.100 <- netsim(nets,
                            init = init_het_ct(),
                            control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.avg.ept.100, here("sims", "may", "simAvgEPT100.rds"))
+saveRDS(sim.avg.ept.100, here("sims", "may2", "simAvgEPT100.rds"))
 rm(sim.avg.ept.100)
 rm(nets)
 
@@ -103,7 +103,7 @@ sim.nc.0 <- netsim(nets,
                     init = init_het_ct(),
                     control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.nc.0, here("sims", "may", "simNc0.rds"))
+saveRDS(sim.nc.0, here("sims", "may2", "simNc0.rds"))
 rm(sim.nc.0)
 
 sim.nc <- netsim(nets,
@@ -111,7 +111,7 @@ sim.nc <- netsim(nets,
                   init = init_het_ct(),
                   control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.nc, here("sims", "may", "simNc.rds"))
+saveRDS(sim.nc, here("sims", "may2", "simNc.rds"))
 rm(sim.nc)
 
 sim.nc.ept <- netsim(nets,
@@ -119,7 +119,7 @@ sim.nc.ept <- netsim(nets,
                       init = init_het_ct(),
                       control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.nc.ept, here("sims", "may", "simNcEPT.rds"))
+saveRDS(sim.nc.ept, here("sims", "may2", "simNcEPT.rds"))
 rm(sim.nc.ept)
 
 sim.nc.ept.high <- netsim(nets,
@@ -127,7 +127,7 @@ sim.nc.ept.high <- netsim(nets,
                            init = init_het_ct(),
                            control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.nc.ept.high, here("sims", "may", "simNcEPThigh.rds"))
+saveRDS(sim.nc.ept.high, here("sims", "may2", "simNcEPThigh.rds"))
 rm(sim.nc.ept.high)
 
 sim.nc.ept.100 <- netsim(nets,
@@ -136,8 +136,37 @@ sim.nc.ept.100 <- netsim(nets,
                           init = init_het_ct(),
                           control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
 
-saveRDS(sim.nc.ept.100, here("sims", "may", "simNcEPT100.rds"))
+saveRDS(sim.nc.ept.100, here("sims", "may2", "simNcEPT100.rds"))
 rm(sim.nc.ept.100)
 rm(nets)
 
 
+
+
+## -------- NO SEX AFTER EPT FLAG -----------------
+#### Concurrency By Sex ####
+nets <- readRDS(here("fits", "calibrated_netests.rds"))
+
+sim.base <- netsim(nets,
+                   param = param_het_ept(epistats, ept.coverage = 0.25, noeptsex = TRUE),
+                   init = init_het_ct(),
+                   control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
+
+saveRDS(sim.base, here("sims", "may2",  "noeptsex", "simBase.rds"))
+rm(sim.base)
+
+nets <- readRDS(here("fits", "calibrated_netests.rds"))
+sim.base100 <- netsim(nets,
+                   param = param_het_ept(epistats, noeptsex = TRUE, ept.coverage = 1,
+                                         ept.provision.pers.ong = 1, ept.provision.main.ong = 1),
+                   init = init_het_ct(),
+                   control = control_het_ct(nsims = 5, nsteps=52*80, ncores=5))
+
+saveRDS(sim.base100, here("sims", "may2",  "noeptsex", "simBase100.rds"))
+rm(sim.base)
+
+rm(nets)
+
+## -------- Add'l Runs -----------------
+## All Previous Partners + All Current Partners 
+## 100 ept coverage w/ same cascade of 
